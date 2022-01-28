3b8568c8706e4b5f15051b58ef73fbd17e8bdab0
Tejeda <tejeda.j086302@stu.sanjac.edu>
2022-01-28 09:40:19 -0600

    added button to html

--- js/main.js
+++ js/main.js
@@ -1,21 +1,20 @@
 //  Pull Data from API
-const getErgastAPI = async function (series, season) {
+const getRaceAPI = async function (series, season) {
     let response = await fetch(`https://ergast.com/api/f1/${series}/${season}/driverStandings.json`);
     let data = await response.json();
     return await data;
 }
 
-// Grabbing the form from the document
 const f1Form = document.getElementById('f1Form');
 
-// function to add list to html
+
 function addToF1List(DriverStanding){
     let f1List = document.querySelector('#f1List');
     let liEl = document.createElement('li');
     liEl.innerText = `Driver name: ${DriverStanding.givenName} position: ${DriverStanding.position} Constructor: ${DriverStanding.ConstructorId}`
     f1List.append(liEl);
 }
-// Event listener for submit
+
 f1Form.addEventListener('submit', async (e) => {
     e.preventDefault();
     let f1FormName = e.target.f1Name.value;
